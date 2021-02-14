9706ed3a246b9e50fdc94f09cf749a0c8035ec99
Ted Romanus <trom@softserveinc.com>
2021-02-14 02:51:23 +0200

    Benchmark dgels

--- main.py
+++ main.py
@@ -7,34 +7,38 @@ from tabulate import tabulate
 import pyximport; pyximport.install()
 import mainx
 
-seed = 42 # random seed
+seeds = [393, 950]
+# seeds = [393, 950, 493, 923, 859, 871, 687, 996, 818, 867] # random seeds
 N = 100 # repeats number
 
 if __name__ == '__main__':
 
     # dimensions = np.array([50, 100, 200, 300, 400])
-    dimensions = np.array([50, 100])
+    dimensions = np.array([100, 200])
     algorithms = np.array(['dgels', 'dgelsy', 'dgelss', 'dgelsd'])
-    types = np.array(['type1', 'type2', 'type3'])
-    # types = np.array(['type1'])
+    # types = np.array([
+    #     'Singular values distributed arithmetically from eps up to 1',
+    #     'Singular values distributed geometrically from eps up to 1',
+    #     '1 singular value at 1 and the other clustered at eps'])
+    types = np.array([
+        'Singular values distributed arithmetically from eps up to 1'])
 
     results = np.zeros((len(types), len(algorithms), len(dimensions)), dtype=np.float)
 
     # benchmark functions
-    for dim_id, dim in enumerate(dimensions):
-        m, n, nrhs, lda, ldb = dim, dim, 1, dim, dim
-        for type_id, _ in enumerate(types):
-            A, b = mainx.generate_matrices(seed, type_id, m, n, nrhs)
-            try:
+    for seed in seeds:
+        for dim_id, dim in enumerate(dimensions):
+            m, n, nrhs, lda, ldb = dim, dim, 1, dim, dim
+            for type_id, _ in enumerate(types):
+                A, b = mainx.generate_matrices(seed, type_id, m, n, nrhs)
                 for alg_id, _ in enumerate(algorithms):
-                    results[type_id][alg_id][dim_id] = timeit("mainx.solve_lss(alg_id, A, b, m, n, nrhs, lda, ldb)", globals=globals(), number=N) / N
-            finally:
+                    results[type_id][alg_id][dim_id] += timeit("mainx.solve_lss(alg_id, A, b, m, n, nrhs, lda, ldb)", globals=globals(), number=N)
                 mainx.free_matrices(A, b)
 
     # print results
     dimensions_annotated = np.insert(dimensions.astype(np.str), 0, ['Alg'])
     for type_id, type_name in enumerate(types):
-        print("type:", type_name)
+        print("Type", type_id+1, "-", type_name)
         results_annotated = np.empty((results.shape[1], results.shape[2]+1), dtype=np.object)
         results_annotated[:,1:] = results[type_id]
         results_annotated[:,0] = algorithms
--- mainx.pyx
+++ mainx.pyx
@@ -1,7 +1,16 @@
+#cython: language_level=3
+
+# python-related
+import sys
+import numpy as np
+import scipy.linalg
+
+# cython-related
 cimport scipy.linalg.cython_lapack as lapack
 # from cpython cimport array
 # import array
 from libc.stdlib cimport malloc, free
+from libc.string cimport memcpy
 
 # def example_func():
 #     cdef array.array a = array.array('f', [
@@ -34,12 +43,75 @@ from libc.stdlib cimport malloc, free
 #     finally:
 #         free(work)
 
-def solve_lss(int alg_id, int A, int b, int m, int n, int nrhs, int lda, int ldb):
-    pass
+def solve_lss(int alg_id, long long A, long long b, int m, int n, int nrhs, int lda, int ldb):
+    cdef unsigned long long A_size = m * n * sizeof(double)
+    cdef unsigned long long b_size = m * nrhs * sizeof(double)
+    cdef double* A_ptr = <double*>malloc(A_size)
+    cdef double* b_ptr = <double*>malloc(b_size)
+    cdef double* work
+    cdef int lwork, info, mn = min(m,n)
+
+    memcpy(A_ptr, <void*>A, A_size)
+    memcpy(b_ptr, <void*>b, b_size)
+
+    if alg_id == 0: # dgels
+        lwork = max(1, mn + max(mn, nrhs))
+        work = <double*>malloc(lwork * sizeof(double))
+        lapack.dgels("N", &m, &n, &nrhs, A_ptr, &lda, b_ptr, &ldb, work, &lwork, &info)
+        free(work)
+    else:
+        pass
+
+    free(A_ptr)
+    free(b_ptr)
 
 def generate_matrices(int seed, int type_id, int m, int n, int nrhs):
-    return 0, 0
+    cdef double* A = <double*>malloc(m * n * sizeof(double))
+    cdef double* b = <double*>malloc(m * nrhs * sizeof(double))
 
-def free_matrices(int A, int b):
+    if not A or not b:
+        raise MemoryError()
+
+    np.random.seed(seed)
+
+    dtype = np.double
+
+    U = scipy.linalg.orth(np.random.randn(m, m))
+    S = np.zeros((m,n))
+    VT = scipy.linalg.orth(np.random.randn(n, n))
+
+    bc = np.random.randn(m, nrhs)
+
+    mn = min(m,n)
+
+    if type_id == 0:
+        # Singular values distributed arithmetically from eps up to 1
+        diag_vals = np.linspace(sys.float_info.epsilon, 1, mn)
+    elif type_id == 1:
+        # Singular values distributed geometrically from eps up to 1
+        diag_vals = np.logspace(sys.float_info.epsilon, 1, mn, base=2) / 2
+    elif type_id == 2:
+        # 1 singular value at 1 and the other clustered at eps
+        diag_vals = sys.float_info.epsilon * np.ones((mn))
+        diag_vals[0] = 1
+    else:
+        raise Exception("unknown type_id")
+
+    S[:mn, :mn] = np.diag(diag_vals)
+
+    Ac = U @ S @ VT
+
+    bf = bc.ravel(order='F')
+    Af = Ac.ravel(order='F')
+
+    for i in range(m*n):
+        A[i] = Af[i]
+
+    for i in range(m*nrhs):
+        b[i] = bf[i]
+
+    return <long long>A, <long long>b
+
+def free_matrices(long long A, long long b):
     free(<void*>A)
     free(<void*>b)
